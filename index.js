3308a1d082d10ead6271fe99e0326059122791fb
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-08-21 14:46:05 -0500

    Adding listen command

--- index.js
+++ index.js
@@ -16,3 +16,5 @@ const PORT = process.env.PORT || 5000;
 app.get('/', (req, res) => {
     res.send("Server is running.");
 });
+
+server.listen(PORT, () => console.log(`Server listening on Port ${PORT}`));
