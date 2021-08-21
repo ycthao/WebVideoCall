5b21abbcd45d69de954be36f3b2aa979683d8397
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-08-21 14:52:53 -0500

    cdefining sockets

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const app = require("express")();
-const server = require("http").createServer(app);
-const cors = require("cors");
+const app = require('express')();
+const server = require('http').createServer(app);
+const cors = require('cors');
 
-const io = require("socket.io")(server, {
+const io = require('socket.io')(server, {
     cors: {
-        origin: "*",
-        methods: ["GET", "PULL"]
+        origin: '*',
+        methods: ['GET', 'PULL']
     }
 });
 
@@ -14,7 +14,23 @@ app.use(cors());
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send("Server is running.");
+    res.send('Server is running.');
+});
+
+io.on('connection', (socket) => {
+    socket.emit('me', socket.id);
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit("callended");
+    });
+
+    socket.on("calluser", ({userToCall, signalData, from, name}) => {
+        io.to(userToCall).emit("calluser", {signal: signalData, from, name});
+    });
+
+    socket.on("answercall", (data) => {
+        io.to(data.to).emit("callaccepted", data.signal);
+    });
 });
 
 server.listen(PORT, () => console.log(`Server listening on Port ${PORT}`));
