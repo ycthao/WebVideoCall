9f3ff37c1d403f34f4757eebda713bb6f4cb26a3
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-08-21 15:39:18 -0500

    adding typography and appbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Typography, AppBar } from '@material-ui/core';
 
 const App = () => {
     return (
         <div>
-            Video Chat App
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">Video Chat</Typography>
+            </AppBar>
+            {/* VideoPlayer */}
+            {/* VideoPlayer */}
         </div>
     );
 };
