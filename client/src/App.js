ebb1eec7c56192693ab7df8e1e7354f79c726e73
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-08-21 22:42:00 -0500

    create components

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import { Typography, AppBar } from '@material-ui/core';
+import React from "react";
+import { Typography, AppBar } from "@material-ui/core";
 
 const App = () => {
-    return (
-        <div>
-            <AppBar position="static" color="inherit">
-                <Typography variant="h2" align="center">Video Chat</Typography>
-            </AppBar>
-            {/* VideoPlayer */}
-            {/* VideoPlayer */}
-        </div>
-    );
+  return (
+    <div>
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Video Chat
+        </Typography>
+      </AppBar>
+      {/* VideoPlayer */}
+      {/* Options -> Notifications */}
+    </div>
+  );
 };
 
 export default App;
