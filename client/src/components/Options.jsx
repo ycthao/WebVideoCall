6c9a225f01a29758beffc6b8a180ccb8e2626089
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-08-22 17:30:30 -0500

    calling imports for options

--- client/src/components/Options.jsx
+++ client/src/components/Options.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import { Button, TextField, Grid, Typography, Container, Paper } from '@material-ui/core';
+import { makesStyles } from '@material-ui/core/styles';
+import { CopyToClipBoard } from 'react-copy-to-clipboard';
+import { Assignment, Phone, PhoneDisabled } from '@material-ui/icons';
+
+import { SocketContext } from '../SocketContext';
 
 const Options = ( { children } ) => {
     return (
