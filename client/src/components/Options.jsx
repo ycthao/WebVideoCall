5362783c2cae810cc9145b004f20ba68b709bf80
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-08-22 17:32:24 -0500

    Adding style to options

--- client/src/components/Options.jsx
+++ client/src/components/Options.jsx
@@ -5,6 +5,38 @@ import { CopyToClipBoard } from 'react-copy-to-clipboard';
 import { Assignment, Phone, PhoneDisabled } from '@material-ui/icons';
 
 import { SocketContext } from '../SocketContext';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+      },
+      gridContainer: {
+        width: '100%',
+        [theme.breakpoints.down('xs')]: {
+          flexDirection: 'column',
+        },
+      },
+      container: {
+        width: '600px',
+        margin: '35px 0',
+        padding: 0,
+        [theme.breakpoints.down('xs')]: {
+          width: '80%',
+        },
+      },
+      margin: {
+        marginTop: 20,
+      },
+      padding: {
+        padding: 20,
+      },
+      paper: {
+        padding: '10px 20px',
+        border: '2px solid black',
+      },
+}))
 
 const Options = ( { children } ) => {
     return (
