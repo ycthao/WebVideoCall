11ff612aa003f4102d6e3cf24933b255d69ff6f3
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-08-22 20:57:06 -0500

    setup notifications

--- client/src/components/Notifications.jsx
+++ client/src/components/Notifications.jsx
@@ -5,9 +5,16 @@ import { SocketContext } from '../SocketContext';
 const Notifications = () => {
     const { answerCall, call, callAccepted} = useContext(SocketContext);
     return (
-        <div>
-            Notifications
-        </div>
+        <>
+            {call.isReceivedCall && !callAccepted && (
+                <div style={{display: 'flex', justifyContent: 'center'}}>
+                    <h1>{call.name} is calling: </h1>
+                    <Button variant="contained" color="primary" onClick={answerCall}>
+                        Answer
+                    </Button>
+                </div>
+            )}
+        </>
     )
 }
 
